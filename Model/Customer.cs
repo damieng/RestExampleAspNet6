c611d5d2958e7d5ed3884fba21864680b3571cba
Damien Guard <damieng@gmail.com>
2022-07-06 17:16:26 +0100

    LastModified support

--- Model/Customer.cs
+++ Model/Customer.cs
@@ -1,17 +1,13 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using RestExample.Infrastructure;
+using System.ComponentModel.DataAnnotations;
 
 namespace RestExample.Model;
 
 /// <summary>
 /// Details about a customer of the system.
 /// </summary>
-public class Customer
+public class Customer : Entity
 {
-    /// <summary>
-    /// Unique identifier used to reference this customer.
-    /// </summary>
-    public Guid Id { get; set; } 
-
     /// <summary>
     /// This customers name.
     /// </summary>
@@ -22,14 +18,4 @@ public class Customer
     /// A <see cref="List{Address}"/> used by this customer.
     /// </summary>
     public List<Address> Addresses { get; set; } = null!;
-
-    /// <summary>
-    /// When this customer was created.
-    /// </summary>
-    public DateTime CreatedAt { get; set; }
-
-    /// <summary>
-    /// When this customer was updated.
-    /// </summary>
-    public DateTime UpdatedAt { get; set; }
 }
--- Model/Order.cs
+++ Model/Order.cs
@@ -1,25 +1,12 @@
-﻿namespace RestExample.Model;
+﻿using RestExample.Infrastructure;
+
+namespace RestExample.Model;
 
 /// <summary>
 /// Details about an order placed.
 /// </summary>
-public class Order
+public class Order : Entity
 {
-    /// <summary>
-    /// Unique reference for this order.
-    /// </summary>
-    public Guid Id { get; set; }
-
-    /// <summary>
-    /// When this order was created.
-    /// </summary>
-    public DateTime CreatedAt { get; set; }
-
-    /// <summary>
-    /// When this order was updated.
-    /// </summary>
-    public DateTime UpdatedAt { get; set; }
-
     /// <summary>
     /// Which <see cref="Customer"/> placed this order.
     /// </summary>
